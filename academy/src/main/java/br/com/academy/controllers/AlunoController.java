60383e752c29d46fa43fa1537209543e7e1e5f39
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-03-31 16:35:01 -0300

    Filtros de pesquisa de dados com JPA e JPQL

--- academy/src/main/java/br/com/academy/controllers/AlunoController.java
+++ academy/src/main/java/br/com/academy/controllers/AlunoController.java
@@ -1,10 +1,13 @@
 package br.com.academy.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.academy.dao.AlunoDao;
@@ -63,6 +66,59 @@ public class AlunoController {
         return "redirect:/alunos-adicionados";
     }
 
+    @GetMapping("/filtro-alunos")
+    public ModelAndView filtroAlunos(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("aluno/pesquisa");
+        return mv;
+    }
 
+    @GetMapping("/alunos-ativos")
+    public ModelAndView listaAlunosAtivos(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("aluno/alunos-ativos");
+        mv.addObject("alunosAtivos", alunoRepositorio.findByStatusAtivo());
+        return mv;
+    }
+
+    @GetMapping("/alunos-inativos")
+    public ModelAndView listaAlunosInativos(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("aluno/alunos-inativos");
+        mv.addObject("alunosInativos", alunoRepositorio.findByStatusInativo());
+        return mv;
+    }
+
+    @GetMapping("/alunos-trancados")
+    public ModelAndView listaAlunosTrancado(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("aluno/alunos-trancados");
+        mv.addObject("alunosTrancados", alunoRepositorio.findByStatusTrancado());
+        return mv;
+    }
+
+    @GetMapping("/alunos-cancelados")
+    public ModelAndView listaAlunosCancelados(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("aluno/alunos-cancelados");
+        mv.addObject("alunosCancelados", alunoRepositorio.findByStatusCancelado());
+        return mv;
+    }
+
+    @PostMapping("/pesquisar-aluno")
+    public ModelAndView pesquisarAluno(@RequestParam(required = false) String nome){
+        ModelAndView mv = new ModelAndView();
+        List<Aluno> listAlunos;
+        if(nome == null || nome.trim().isEmpty()){
+            listAlunos = alunoRepositorio.findAll();
+        }else{
+            listAlunos = alunoRepositorio.findByNomeContainingIgnoreCase(nome);
+        }
+
+        mv.addObject("ListaAlunos", listAlunos);
+        mv.setViewName("aluno/pesquisa-resultado");
+        return mv;
+   
+    }
 
 }
--- academy/src/main/java/br/com/academy/dao/AlunoDao.java
+++ academy/src/main/java/br/com/academy/dao/AlunoDao.java
@@ -1,9 +1,26 @@
 package br.com.academy.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.academy.model.Aluno;
 
 public interface AlunoDao extends JpaRepository<Aluno, Integer>{
     
+    @Query("SELECT p FROM Aluno p WHERE p.status = 'ATIVO' ")
+    public List<Aluno> findByStatusAtivo();
+
+    @Query("SELECT p FROM Aluno p WHERE p.status = 'INATIVO' ")
+    public List<Aluno> findByStatusInativo();
+
+    @Query("SELECT p FROM Aluno p WHERE p.status = 'TRANCADO' ")
+    public List<Aluno> findByStatusTrancado();
+
+    @Query("SELECT p FROM Aluno p WHERE p.status = 'CANCELADO' ")
+    public List<Aluno> findByStatusCancelado();
+
+    public List<Aluno> findByNomeContainingIgnoreCase(String nome);
+
 }
